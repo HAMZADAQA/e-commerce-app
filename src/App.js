442e79819a747f82c1321b276be08d8ecc6b98a6
Hamza Daqa <hamza.daqa@nibw.es>
2020-11-22 20:33:06 +0100

    share code on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import data from './data'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="grid-container">
+    <header class="row">
+      <div>
+        <a class="brand" href="/">amazon</a>
+      </div>
+      <div>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
+
+      </div>
+    </header>
+    <main>
+      <div class="row center">
+        { data.products.map(product => (
+        <div key={product._id} class="card">
+          <a href={`/product/${product._id}`}>
+            <img class="medium" src={product.image} alt={product.name} />
+          </a>  
+          <div class="card-body">
+            <a href={`/product/${product._id}`}>
+              <h2>{product.name} </h2>
+            </a>
+          
+          <div class="rating">
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+            <span>
+              <i class="fa fa-star"></i>
+            </span>
+          </div>
+          <div class="price">
+              ${product.price}
+          </div>
+        </div>
+      </div>
+
+        ))}
+
+      </div>
+    </main>
+    <footer class="row center">
+      All right reserved
+    </footer>
+  </div>
   );
 }
 
